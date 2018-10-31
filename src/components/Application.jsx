8efde5b8e14378b7268adcb4130ea5b4c9e2319a
Henrik Solgaard <h@solg.no>
2018-10-31 23:22:49 +0100

    Create a simple layout based on Bootstrap.

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -15,9 +15,25 @@ class Application extends React.Component {
     render() {
         return (
             <div>
-                <h1>Application</h1>
-                <img src="assets/png/test.png"/>
-                <a href="/login" onClick={() => {this.logOut()}}>Logout</a>
+                <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                    <a className="navbar-brand" href="#">Application</a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav mr-auto">
+                            <li className="nav-item active">
+                                <a className="nav-link" href="#">Page 1 <span className="sr-only">(current)</span></a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Page 2</a>
+                            </li>
+                        </ul>
+                        <span className="nav-item">
+                            <a href="/login" onClick={() => {this.logOut()}}>Logout</a>
+                        </span>
+                    </div>
+                </nav>
             </div>
         )
     }
